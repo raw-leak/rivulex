d2c7e512c8617d9709915216addffa729291297c
Mykhaylo Gusak <michaelxxi@outlook.es>
2024-07-22 21:43:40 +0200

    feat: :sparkles: refactored configs layers

--- lib/config/publisher-config.ts
+++ lib/config/publisher-config.ts
@@ -1,9 +1,55 @@
 import { Headers } from "../types";
 
-export interface PublisherConfig<P extends Record<any, any> = any, H extends Record<any, any> = any> {
+/**
+ * Callback type for successful event publishing.
+ * @template P - The type of the payload.
+ * @template H - The type of the headers.
+ */
+export type PublishSuccessCallback<P extends Record<any, any>, H extends Record<any, any>> = (
+    data: {
+        id: string;
+        headers: Headers<H>;
+        action: string;
+        payload: P;
+        group: string;
+    }
+) => void;
+
+/**
+ * Callback type for failed event publishing.
+ * @template H - The type of the headers.
+ */
+export type PublishErrorCallback<H extends Record<any, any>> = (
+    data: {
+        headers: Headers<H>;
+        action: string;
+        error: Error;
+    }
+) => void;
+
+/**
+ * Configuration object for the Publisher class.
+ * @template P - The type of the payload.
+ * @template H - The type of the headers.
+ */
+export interface PublisherConfig {
+    /**
+     * The Redis stream channel to publish events to.
+     */
     channel: string;
+
+    /**
+     * The consumer group to associate with the events.
+     */
     group: string;
 
-    onMessagePublished?: (data: { id: string, headers: Headers<H>, action: string, payload: P }) => void;
-    onPublishFailed?: (data: { headers: Headers<H>, action: string, error: Error }) => void;
+    /**
+     * Callback to be invoked when an event is successfully published.
+     */
+    onMessagePublished?: PublishSuccessCallback<any, any>;
+
+    /**
+     * Callback to be invoked when publishing an event fails.
+     */
+    onPublishFailed?: PublishErrorCallback<any>;
 }
