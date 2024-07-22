9d3fa02c874d4ce469192311f28bbecb3a6560bf
Mykhaylo Gusak <michaelxxi@outlook.es>
2024-07-22 21:43:54 +0200

    feat: :sparkles: refactored consumer layers

--- lib/consumers/live.consumer.ts
+++ lib/consumers/live.consumer.ts
@@ -2,6 +2,18 @@ import { Formatter } from "../formatter/formatter";
 import { Processor } from "../processor/processor";
 import { ChannelsHandlers, RawEvent, RedisClient } from "../types";
 
+/**
+* Configuration object for the `LiveConsumer` class.
+* 
+* @interface
+* 
+* @member {string} clientId - (REQUIRED) The unique identifier for the consumer instance.
+* @member {string[]} channels - (REQUIRED) An array of channel names (stream names) to connect to.
+* @member {string} group - (REQUIRED) The name of the consumer group that the consumer will join.
+* @member {number} retries - (REQUIRED) The maximum number of retries for processing an event before considering it failed.
+* @member {number} count - (REQUIRED) The number of events to fetch from Redis in each request.
+* @member {number} block - (REQUIRED) The time, in milliseconds, that the consumer will block and wait for new events before returning control.
+*/
 export interface LiveConsumerConfig {
     clientId: string;
     channels: Array<string>;
@@ -11,6 +23,21 @@ export interface LiveConsumerConfig {
     block: number;
 }
 
+/**
+* A class for consuming live events from Redis streams.
+* 
+* The `LiveConsumer` class listens to the specified Redis streams and processes events 
+* according to the configuration provided. It supports blocking reads, consumer groups, 
+* and automatic retries.
+* 
+* @class
+* 
+* @param {LiveConsumerConfig} config - Configuration object containing settings for the consumer.
+* @param {RedisClient} redis - The Redis client instance used for interacting with Redis streams.
+* @param {Console} logger - The logging instance used for outputting information, warnings, and errors.
+* @throws {Error} Throws an error if the Redis client is missing or invalid, if the channels array is empty or not provided, or if the group parameter is missing.
+* 
+*/
 export class LiveConsumer {
     private enabled = false;
     private processor: Processor;
