b973c46468ff93787eef785529ae3c870e1a32f5
Mykhaylo Gusak <michaelxxi@outlook.es>
2024-07-29 11:31:05 +0200

    feat: :wrench: jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,12 +3,7 @@ module.exports = {
     testEnvironment: 'node',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest',
+        '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json', }],
     },
     testMatch: ['**/*.spec.(ts|tsx)', '**/*.e2e.(ts|tsx)'],
-    globals: {
-        'ts-jest': {
-            tsconfig: 'tsconfig.json',
-        },
-    },
 };
